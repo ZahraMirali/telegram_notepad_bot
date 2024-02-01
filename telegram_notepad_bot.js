55982af9689e3d1b2e8e3800847461a8129e8596
Zahra Mirali <z.mirali1997@gmail.com>
2024-02-01 21:02:49 +0100

    add /end

--- telegram_notepad_bot.js
+++ telegram_notepad_bot.js
@@ -4,6 +4,8 @@ const { TOKEN } = require("./token");
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
+let notepadData = {};
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(
@@ -12,33 +14,39 @@ bot.onText(/\/start/, (msg) => {
   );
 });
 
+bot.onText(/\/end/, (msg) => {
+  const chatId = msg.chat.id;
+
+  for (const title in notepadData) {
+    fs.appendFileSync("notepad.txt", notepadData[title]);
+  }
+
+  notepadData = {};
+
+  bot.sendMessage(chatId, "All items written to the notepad file.");
+});
+
 bot.on("message", (msg) => {
   if (msg.forward_origin) {
     const chatId = msg.chat.id;
-
     const text = getText(msg);
-
-    fs.appendFile("notepad.txt", text, (err) => {
-      if (err) {
-        bot.sendMessage(chatId, "Error saving forwarded message to Notepad.");
-      } else {
-        bot.sendMessage(chatId, text.split(":")[0]);
-      }
-    });
+    notepadData[text.split(":")[0]] = text;
+    bot.sendMessage(chatId, Object.keys(notepadData).length);
   }
 });
 
 function getText(msg) {
   let text = "";
-  if (msg.forward_origin.chat.username === "Vocabulix" && msg.caption) {
-    const captionLines = msg.caption.split("\n");
-    let title = captionLines[0].replace(/🔵/g, "").trim();
-    title = title[0].toUpperCase() + title.slice(1);
-    const description = captionLines[2].replace(/♦️/g, "").trim();
-    const example = captionLines[5].split('"')[1].trim();
+  try {
+    if (msg.forward_origin.chat.username === "Vocabulix" && msg.caption) {
+      const captionLines = msg.caption.split("\n");
+      let title = captionLines[0].replace(/🔵/g, "").trim();
+      title = title[0].toUpperCase() + title.slice(1);
+      const description = captionLines[2].replace(/♦️/g, "").trim();
+      const example = captionLines[5].split('"')[1].trim();
 
-    text = `${title}: ${description}. ${title}. ${title}. ${title}. \nExample: ${example}\n\n`;
-  }
-
+      text = `${title}: ${description}. ${title}. ${title}. ${title}. \nExample: ${example}\n\n`;
+    }
+  } catch (error) {}
   return text;
 }
